2d859ab6b4b90aa4c43b616531a1c852aea08787
Jesus <bep010795@gmail.com>
2022-10-04 20:37:25 -0500

    Creacion de formulario

--- src/app.py
+++ src/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    return redirect( url_for('login') )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['username'])
+        print(request.form['password'])
+        return render_template('auth/login.html')
+        
     return render_template('auth/login.html')
